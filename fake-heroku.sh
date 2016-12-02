f97e9bc517dbc4ce39342d3bbd718b5a6262239f
Joel Franusic <joel.franusic@okta.com>
2016-12-01 16:29:16 -0800

    Better error message

--- fake-heroku.sh
+++ fake-heroku.sh
@@ -2,7 +2,9 @@ if [ ! -d "venv" ]; then
     if hash virtualenv; then
         virtualenv venv
     else
-        echo >&2 "I require virtualenv but it's not installed.  Aborting.";
+        echo >&2 "I require virtualenv but it's not installed.  ";
+        echo >&2 "Install virtualenv with: '$ pip install virtualenv'";
+        echo >&2 "Aborting.";
         exit 1;
     fi
 fi
